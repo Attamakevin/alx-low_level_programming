cd3f3a05bd009997149fe852a862918e80437030
attama <kevinejikez@gmail.com>
2022-09-08 15:09:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+/*initializing main with void*/
 int main (void)
 {
 	/*initializing string array*/
 	char str[] = "Programming is like building a multilingual puzzle";
 	/*writing to stdout*/
 	puts(str);
-	return 0;
+	return (0);
 }
