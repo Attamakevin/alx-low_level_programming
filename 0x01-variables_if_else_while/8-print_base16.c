81718434d9f081d429a81aa9409b94b23bdf9393
attama <kevinejikez@gmail.com>
2022-09-10 08:38:12 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,12 @@
 int main(void)
 {
 	int num;
+	char ch;
 
 	for (num = '0'; num <= '9'; num++)
 		putchar(num);
-
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }
