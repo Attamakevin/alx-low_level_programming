5152d06b7661794d4a6e28d3a3a42a5b8c4c232d
attama <kevinejikez@gmail.com>
2022-09-12 13:00:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  *  * main - Entry point
- *
- *   *
- *
  *    * Return: (1) (Success)
  */
 int main(void)
 {
-	printf( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
