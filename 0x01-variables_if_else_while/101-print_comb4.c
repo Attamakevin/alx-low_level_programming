8843c33ea447bd79e7ae5e544d7062e66b925251
attama <kevinejikez@gmail.com>
2022-09-11 20:09:30 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,24 +8,18 @@ int main(void)
 	int a,b,c;
 
 	for (a = '0'; a < '8'; a++)
-	{
 		for (b = a + 1; b < '9'; b++)
-		{
 			for (c = b + 1; c <= '9'; c++)
-			{
-				if (a != b && b !=c && c !=a)
-				putchar((a % 10) + '0');
-				putchar((b % 10) + '0');
-				putchar((c % 10) + '0');
-			
-				if (a == 7 && b == 8 && c == 9)
-					continue;
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		}
-putchar('\n');
-	
+	{
+	if (a != b && b != c && a != c)
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	if (a == 7 && b == 8 && c == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
 return (0);
 }
