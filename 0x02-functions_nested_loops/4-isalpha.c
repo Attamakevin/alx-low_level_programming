bcb9e673c07e7dab9e184d31b6f24c2d43944b03
attama <kevinejikez@gmail.com>
2022-09-13 17:35:02 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - function to check if a letter c is a lower oe upper case
+ *  @c: is the letter used for the arguement
+ *  Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= A && C <= Z))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
