5e20f04c4f5732187d77aec144a4de9a3a24cb34
attama <kevinejikez@gmail.com>
2022-09-15 12:35:45 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 	{
 	if ((year % 400 == 0 || year % 4 == 0) && year % 100 != 0)
 		{
-	if (month >= 2 && day >= 60)
+	if (month > 2 && day >= 60)
 	{
 	day++;
 	}
@@ -28,7 +28,7 @@
 	else
 	{
 	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
+	printf("Remaining days: %d\n", 366 - day);
 	}
 	}
 	}
