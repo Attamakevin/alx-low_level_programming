07e8099ebe3041b460870bbb7bc7c40a0bcecd3a
attama <kevinejikez@gmail.com>
2022-09-15 14:30:23 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks is a letter is an upper case
+ * Return: return 1 on success and 0 if false
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{return (1);
+	}
+	else
+	{return (0);
+}
