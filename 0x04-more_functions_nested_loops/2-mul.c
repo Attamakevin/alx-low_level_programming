0079d31a450fe1b06772006c3f9d834962fdf6a3
attama <kevinejikez@gmail.com>
2022-09-15 15:55:12 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - function multiplied two integers together
+ * @a  : one of the parameters for testing the arguement
+ * @b : one of the parameters for testing the arguement
+ * Return: 0
+ * mull: return value
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+		return (mul);
+}
