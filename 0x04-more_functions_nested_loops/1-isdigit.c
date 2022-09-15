49a579009badd3d64f54024b078f78c42985226b
attama <kevinejikez@gmail.com>
2022-09-15 15:04:49 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - function checks for 0 through 9 and
+ * @c : is the parameter for testing the arguement
+ * Return: return 1 on success and 0 if it fail
+ */
+int _isdigit(int c)
+{
+	if (c == 0; &&c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
