0059c2b64f9ebcd6c9b391f50614b55ff06efb63
attama <kevinejikez@gmail.com>
2022-09-16 04:49:02 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_number - is a function prints an  integer
+ * @n : int to test the parameter
+ */
+void print_number(int n)
+{
+	int Z;
+	int h;
+	int c = 1;
+	int d = n % 10;
+
+	n /= 10;
+	Z = n;
+	if (d < 0)
+	{
+		d *= -1, Z *= -1, n *= -1;
+		_putchar('-');
+	}
+	if (Z > 0)
+		while (Z / 10 != 0)
+		{
+			Z /= 10, c *= 10;
+				while (c > 0)
+				{
+					h = n / c;
+						_putchar('0' + h);
+						n -= h * c;
+						c /= 10;
+				}
+		}
+	_putchar('0' + d);
+}
