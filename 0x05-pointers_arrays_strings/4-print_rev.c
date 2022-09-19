7ec858272c684fd018837bf36d073d526cf36cb9
attama <kevinejikez@gmail.com>
2022-09-19 15:50:53 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,18 @@
 */
 void print_rev(char *s)
 {
-	while (*s != '\0' && *s >= 0)
-	*s++;
-	_putchar(*s--);
+int len = 0, i;
+
+
+
+	while (s[i++])
+
+		len++;
+
+
+
+	for (i = len - 1; i >= 0; i--)
+
+		_putchar(s[i]);
 	_putchar('\n');
 }
