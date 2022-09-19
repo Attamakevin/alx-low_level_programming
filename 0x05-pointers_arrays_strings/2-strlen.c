bbddad1cd7c9b4c62429d49f2dba0cc885c97f2b
attama <kevinejikez@gmail.com>
2022-09-19 07:52:10 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+* _strlen - funtion that calculate the lentgth of a string
+* @s : input
+* Return:  length.
+*/
+int _strlen(char *s)
+{
+	int lenth = 0
+
+	while (s * != '\0')
+	*s++;
+	lenth++;
+	return (lenth);
+}
