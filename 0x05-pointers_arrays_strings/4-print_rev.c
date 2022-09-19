0184966815193eaf3058052fdd1fc72f9293a9a1
attama <kevinejikez@gmail.com>
2022-09-19 15:39:05 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_rev - a function that prints string in reverse form
+* @s : input
+* return: return 0
+*/
+void print_rev(char *s)
+{
+	while (*s != '\0' && *s <= 0)
+	_putchar(*s--);
+	_putchar('\n');
+}
