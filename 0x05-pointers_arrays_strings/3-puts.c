8ca274a184fd16f5798ca0fc656c7a5e8bd22473
attama <kevinejikez@gmail.com>
2022-09-19 15:31:13 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _puts - a function that prints a string
+* @str : input
+* return: 0
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	_putchar(*str++);
+	_putchar('\n');
+}
