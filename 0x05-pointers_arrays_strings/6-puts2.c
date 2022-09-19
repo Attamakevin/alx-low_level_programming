fa719b8d02e93996aca54ce87b5c49f795013e41
attama <kevinejikez@gmail.com>
2022-09-19 16:35:05 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* puts2 - a function that prints every charcter of str
+* starting from first
+* @str : input
+* return : 0
+*/
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	for (i = 0; i < '\0'; i++)
+	{
+		_putchar(str[i]);
+	_putchar('\n');
+	}
+}
