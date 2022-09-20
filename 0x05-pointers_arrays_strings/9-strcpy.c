218b6918e993f14c47a80fc84ed751760cd303e6
attama <kevinejikez@gmail.com>
2022-09-19 19:16:02 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strcpy - function that copy pointed t
+* @src : copy from
+* @dest : copy to
+* Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int n;
+
+	while (i = 0, i < n)
+	{
+	while (src[i] != '\0')
+	{
+	i++;
+	}
+	for (; i < n; i++)
+	{
+	dest[i] = src[i];
+	dest[i] = '\0';
+	}
+	}
+}
