6ca4ed95891aa3f31f8eb44d27b6ac23e8c7ee5a
attama <kevinejikez@gmail.com>
2022-09-22 17:30:15 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*string_toupper - function to convert lowercase character to uppercase
+*@a: input
+*Return: upper case
+*/
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+		a[i] = a[i] - 32;
+		}
+	}
+}
