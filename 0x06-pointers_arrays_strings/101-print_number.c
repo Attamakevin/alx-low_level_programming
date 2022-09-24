46676d927fe9d57331cadbe4e81cebcda6fb23c1
attama <kevinejikez@gmail.com>
2022-09-24 04:17:50 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,9 +11,13 @@ void print_number(int n)
 	if (n < 0)
 	{
 	_putchar('-');
-	num = (-num);
+	num = (-n);
 	}
-	else if (n > 0)
+	else if (n >= 0 && n < 10)
+	{
+	_putchar('0'+ n);
+	}
+	else if (n >= 10)
 	{
 	_putchar((num % 10) + '0');
 	}
