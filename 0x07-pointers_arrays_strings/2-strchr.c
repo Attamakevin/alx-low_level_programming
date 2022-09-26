acc984e2981655faa176a5bcafa1521f2b3edfd1
attama <kevinejikez@gmail.com>
2022-09-26 15:45:20 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*strchr - function to search for occurrence of character in a string
+*@s: string to be searched
+*@c: target charcter
+*Return: pointer to the target string
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+	i++;
+	{
+	if (s[i] == c)
+	return (&s[i]);
+	
+	else
+	return (NULL);
+	}
+return (0);
+}
