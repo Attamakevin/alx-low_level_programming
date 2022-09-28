dbbd420b2b7482a96212b22a6b3424ceb1a9429b
attama <kevinejikez@gmail.com>
2022-09-28 15:11:35 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts_recursion - function to print a string
+* @s: string input
+* Return: 0
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	_putchar('\n');
+	return;
+	}
+	else
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
+
