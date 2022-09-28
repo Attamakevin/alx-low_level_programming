80acea77cab5c573d811e7e0598c3ba3032e07c5
attama <kevinejikez@gmail.com>
2022-09-28 16:39:03 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,10 +8,22 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	return (-1);
-	else if (x == 0)
-	return (0);
-	else if (x == 1 || y == 0)
-	return (1);
-	return (y * _pow_recursion(x, y - 1));
+
+		return (-1);
+
+
+
+	if (x == 0)
+
+		return (0);
+
+
+
+	if (x == 1 || y == 0)
+
+		return (1);
+
+
+
+	return (x * _pow_recursion(x, y - 1));
 }
