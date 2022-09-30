2c0004f5dd2452bf9aa5e81fb64b0f19aae90124
attama <kevinejikez@gmail.com>
2022-09-30 15:03:32 -0700

    updated

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,9 @@
-#ifndef MAIN-H
-#define MAIN-H
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -39,4 +43,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
 #endif
