5f5d556fefa0f8dae2943aef4f39b4d1cf21d5c5
attama <kevinejikez@gmail.com>
2022-09-30 15:50:04 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: pointer to array of arguments
  * Return: 0 on success
  */
-int main(int argc, char*arg[])
+int main(int argc, char*arg[]__attribute__((unused)))
 {
 	if (argc > 0)
 	printf("%d\n", argc -1);
