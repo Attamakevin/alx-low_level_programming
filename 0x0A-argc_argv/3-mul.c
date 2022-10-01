faf35b47a15f1c69ddd5dd7effa13535de5ed049
attama <kevinejikez@gmail.com>
2022-09-30 17:49:39 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - function prints multiplication of two numbers
+* @argc: argument counter
+* @argv: numbers to multiply
+* Return: return 0 on success, and 1 if numbers are less
+* than two
+*/
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+	printf("error\n");
+	return (1);
+	}
+	printf("%d\n", atoi (argv[1]) * atoi (argv[2]));
+	return (0);
+}
