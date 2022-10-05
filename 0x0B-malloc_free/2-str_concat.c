27546e8ede9499dfe1ec6875f57e1da2a5ba7867
attama <kevinejikez@gmail.com>
2022-10-04 18:34:24 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,15 +18,17 @@ char *str_concat(char *s1, char *s2)
 	return (" ");
 	if (s2 == NULL)
 	return (" ");
-	len1 = 0;
-	i = 0;
+
+	len1 = 0, i = 0;
 	while (*(s1 + i))
 		len1++;
 		i++;
+
 	len2 = 0, j = 0;
 	while (*(s2 + j))
 		len2++;
 		j++;
+
 	len2++;
 	concate = malloc(sizeof(char) * (len1 + len2));
 	if (concate == NULL)
