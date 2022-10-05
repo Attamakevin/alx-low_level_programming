0ed23510dcbe869d7db3090ce45edd4e9349afd4
attama <kevinejikez@gmail.com>
2022-10-04 21:50:18 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -51,8 +51,8 @@ char **strtow(char *str)
 			n = 0;
 			while (str[n] != ' ')
 				n++;
-			/*strDup[i] = malloc(sizeof(char) * n);
-			if (strDup[i] == NULL)*/
+			strDup[i] = malloc(sizeof(char) * n);
+			if (strDup[i] == NULL)
 			{
 				while (--i >= 0)
 					free(strDup[--i]);
