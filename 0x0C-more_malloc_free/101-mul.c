75ab5da4e042cf6b03300fea0c2e14f0da94f657
attama <kevinejikez@gmail.com>
2022-10-08 06:55:16 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	if (_isdigit(argv[1]) == 0)
-		printf("%s\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
