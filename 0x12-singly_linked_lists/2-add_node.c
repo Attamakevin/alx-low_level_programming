d57eab3a0488316b28d9434ed98323d65909200a
attama <kevinejikez@gmail.com>
2022-10-20 20:45:11 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _strlen - function to get the length of a string
+* @s: string parameter
+* Return: return length of string
+*/
+int _strlen(const chat *s)  
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	return (i);
+}
+/**
+* add_node - function to add a string at the beginning of list_t
+* @str: string parameter
+* @*head: pointer to the first node in the list
+* Return: return updated list_t
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+	while (newnode)
+	{
+		newnode->str = strdup(str);
+		newnode->len = _strlen(str);
+		newnode->next = *head;
+		*head = newnode;
+	}
+return (newnode);
+}
