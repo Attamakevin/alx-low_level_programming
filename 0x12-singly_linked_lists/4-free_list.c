19f8651d598e45208f874e982630e2351e70a8c4
attama <kevinejikez@gmail.com>
2022-10-21 21:13:39 -0700

    fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,34 +11,18 @@
 
 
 void free_list(list_t *head)
-
 {
-
 	list_t *tmp;
 
-
-
 	if (head == NULL)
-
 		return;
-
 	while (head->next != NULL)
-
 	{
-
 		tmp = head->next;
-
 		free(head->str);
-
 		free(head);
-
 		head = tmp;
-		free(tmp);
-
 	}
-
 	free(head->str);
-
 	free(head);
-
 }
