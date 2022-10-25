119dba0151329ffe84e5b8e77a93d805c18962c2
attama <kevinejikez@gmail.com>
2022-10-24 18:45:30 -0700

    fixed

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,7 +4,7 @@
 * free_listint - function to free a linked list
 * @head: pointer to the first node in the list
 * Return: return zero
-*/ 
+*/
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
