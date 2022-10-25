b6da89915fcc62f055ef6efdfc0831e1134280df
attama <kevinejikez@gmail.com>
2022-10-24 19:17:40 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,12 @@
 void free_listint2(listint_t **head)
 {
 	if (!head || !*head)
+	return;
 
 	while (*head)
 	{
 	listint_t *tmp = *head;
-	*head = (*head)->next;
+	(*head) = (*head)->next;
 	free(tmp);
 	}
 	*head = 0;
