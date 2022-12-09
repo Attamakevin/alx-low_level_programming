fa22387619d3158c8ebf25bc8b9b5208a1ec5a3c
attama <kevinejikez@gmail.com>
2022-12-09 14:28:09 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,2 +1,21 @@
 #include "lists.h"
-size_t dlistint_len(const dlistint_t *h);
+/**
+* dlistint_len - function to find length of list
+* @h: pointer to the first node
+* Return: length of the list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+ size_t i;
+const dlistint_t *temp = h;
+
+ if (!h || h == 0)
+	return (0);
+ i = 0;
+while (temp)
+{
+	temp = temp->next;
+	i++;
+}
+return (i);	
+}
