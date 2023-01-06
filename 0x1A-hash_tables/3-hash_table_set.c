d15ea912242f82a96fc4b8b3afbd2b1171d9a022
attama <kevinejikez@gmail.com>
2023-01-06 11:34:07 -0800

    hot fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,12 +6,12 @@
 * @value: the value at key index
 * Return: return 1  on success and 0 if fail
 */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value) 
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index;
 	hash_node_t *new_node, *current;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL) 
+	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
 	{
 		return (0);
 	}
