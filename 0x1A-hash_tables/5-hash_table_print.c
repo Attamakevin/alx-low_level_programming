6a6d0c5f0651f9b7f757f261f0a071f591c540d0
attama <kevinejikez@gmail.com>
2023-01-07 17:52:57 -0800

    fixed

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - a function to print hash table
+* @ht: the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	int flag = 0;
+	hash_node_t *node;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			if (flag == 1)
+				printf(", ");
+			while (node)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+			flag = 1;
+			}
+		flag = 1;
+		}
+	printf("}\n");
+}
