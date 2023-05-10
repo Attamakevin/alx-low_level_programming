202427e2c0e4452ce8076088b113d6dcfc98caff
attamakevin <kevinejikez@gmail.com>
2023-05-09 19:06:12 -0700

    0-linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+/**
+ * linear_search - a function to implement linear search
+ * @array: the array to search through
+ * @size: the size of the array
+ * @value: the value to search for
+ * Return: the index of the value in the array or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int found = 0;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
+			printf("found %d at index %ld\n", value, i);
+			found = 1;
+			break;
+		}
+		printf("value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	if (found == 0)
+	{
+		return (-1);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,9 @@
+<h1>0x1E. C - Search Algorithms</h1>
+<h2>Learning Objectives</h2>
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+<h2>General</h2>
+What is a search algorithm
+What is a linear search
+What is a binary search
+What is the best search algorithm to use depending on your needs
